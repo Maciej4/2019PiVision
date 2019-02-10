019a7af6d2a6177a5037d8ebf4f7bb0850bd36f8
Maciej4 <m@maciej.me>
2019-02-09 21:04:15 -0800

    changed name

--- vision.py
+++ vision.py
@@ -8,34 +8,34 @@ cap.set(3, 160)
 cap.set(4, 120)
 
 def dist(a, b):
-    
+
     # Find distance between two points
     return math.sqrt((a[0]-b[0])*(a[0]-b[0])+(a[1]-b[1])*(a[1]-b[1]))
-    
+
 def average(a, b):
-    
+
     # Average two positions
     cx = (a[0]+b[0])/2
     cy = (a[1]+b[1])/2
     return [cx, cy]
-    
+
 def vecMag(vec, mag):
     strPt = vec[0]
-    
+
     #Calculate vector end point from 0,0
     vec[1][0] = vec[1][0] - strPt[0]
     vec[1][1] = vec[1][1] - strPt[1]
-    
+
     curMag = dist((0, 0), vec[1])
-    
+
     vec[1][0] = (vec[1][0]/curMag)*mag + strPt[0]
     vec[1][1] = (vec[1][1]/curMag)*mag + strPt[1]
-    
+
     return vec
-    
+
 
 def findline(image):
-    
+
     # Find contours
     (cnts, _) = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     if len(cnts) > 0:
@@ -45,11 +45,11 @@ def findline(image):
 
 while(1):
     startTime = time.time();
-        
+
     # Take each frame
     _, frame = cap.read()
     #frame = cv2.imread('test2.jpg')
-    
+
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
@@ -62,38 +62,38 @@ while(1):
 
     marker = findline(mask)
     if marker is not None and marker[1][0] > 0:
-        box = np.int0(cv2.cv.BoxPoints(marker))
-        
+        box = np.int0(cv2.boxPoints(marker))
+
         # Calc side lengths
         ab = dist(box[0], box[1])
         bc = dist(box[1], box[2])
         cd = dist(box[2], box[3])
         da = dist(box[3], box[0])
-        
+
         e = average(box[0], box[1])
         f = average(box[2], box[3])
         ef = average(e, f)
-        
+
         abcd = [average(box[0], box[1]), average(box[2], box[3])]
         bcda = [average(box[1], box[2]), average(box[3], box[0])]
-        
+
         cenPts = abcd
-        
+
         #if(ab < bc):
         #    print(abcd)
         #else:
         #    print(bcda)
         #    cenPts = bcda
-            
+
         if(ab > bc):
             cenPts = bcda
-            
+
         #print(vecMag(abcd, 2000))
-        
+
         #print(frame.shape[:2])
-        
+
         centerPoint = average(average(box[0], box[1]), average(box[2], box[3]))
-        
+
         #print("center point: ", centerPoint)
         focalLength = (64 * 34) / 13
         pixelLength = dist(cenPts[0], cenPts[1]);
@@ -104,24 +104,24 @@ while(1):
         y = cmDistance * math.cos(angleFromCenter * 0.0174)
         print("distance cm: ", round(cmDistance, 3), "angle deg: ", round(angleFromCenter, 3))
         print("x: ", round(x, 3), "y: ", round(y, 3))
-        
+
         #print("pixel length: ", pixelLength)
-        
+
         vecB = vecMag(cenPts, 500)
-        
+
         #cv2.rectangle(frame, (10, 10), (26, 22), (0, 0, 255), 2)
         #cv2.line(frame, (int(vecB[0][0]/10), int(vecB[0][1]/10)), (int(vecB[1][0]/10), int(vecB[1][0]/10)), (0, 0, 255), 2)
         #cv2.line(frame, (int(cenPts[0][0]/10+10), int(cenPts[0][1])/10+10),
         # (int(cenPts[1][0]/10+10), int(cenPts[1][1])/10+10), (255, 255, 0), 2)
         #cv2.line(frame, (int(cenPts[0][0]), int(cenPts[0][1])), (int(cenPts[1][0]), int(cenPts[1][1])), (255, 0, 0), 2)
         cv2.drawContours(frame, [box], -1, (0, 0, 255), 2)
-    
+
     # Display image
     cv2.imshow('frame',frame)
-    
+
     # Output time in milliseconds of processing time
     print("runtime ms: ", round((time.time()-startTime)*1000))
-    
+
     # esc to kill program
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
