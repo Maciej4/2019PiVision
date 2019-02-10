e4361d2a178a10deac741063e0da6d625ad7ab5d
Maciej4 <m@maciej.me>
2019-02-09 21:58:31 -0800

    fixed camelCase

--- vision.py
+++ vision.py
@@ -22,58 +22,46 @@ def dist(a, b):
     return math.sqrt((a[0]-b[0])*(a[0]-b[0])+(a[1]-b[1])*(a[1]-b[1]))
 
 def average(vec2s):
-    xtotal = 0
-    ytotal = 0
+    xTotal = 0
+    yTotal = 0
     for pos in vec2s:
-        xtotal += pos[0]
-        ytotal += pos[1]
-    cx = xtotal / len(vec2s)
-    cy = ytotal / len(vec2s)
+        xTotal += pos[0]
+        yTotal += pos[1]
+    cx = xTotal / len(vec2s)
+    cy = yTotal / len(vec2s)
     return [cx, cy]
 
-def boxcenx(box):
-    xtotal = 0
+def boxCenX(box):
+    xTotal = 0
     for pos in box:
-        xtotal += pos[0]
-    cx = xtotal / len(box)
+        xTotal += pos[0]
+    cx = xTotal / len(box)
     return cx
 
 def boxAng(box):
-    boxab = dist(box[0], box[1])
-    boxbc = dist(box[1], box[2])
-    if(boxab < boxbc):
-        boxab, boxbc = boxbc, boxab
-        boxangle = math.atan2((box[1][1] - box[2][1]), (box[1][0] - box[2][0]))
+    boxAB = dist(box[0], box[1])
+    boxBC = dist(box[1], box[2])
+    if(boxAB < boxBC):
+        boxAB, boxBC = boxBC, boxAB
+        boxAngle = math.atan2((box[1][1] - box[2][1]), (box[1][0] - box[2][0]))
     else:
-        boxangle = math.atan2((box[0][1] - box[1][1]), (box[0][0] - box[1][0]))
-    return math.degrees(boxangle)
+        boxAngle = math.atan2((box[0][1] - box[1][1]), (box[0][0] - box[1][0]))
+    return math.degrees(boxAngle)
 
-def vecMag(vec, mag):
-    strPt = vec[0]
+def longSide(box):
+    boxAB = dist(box[0], box[1])
+    boxBC = dist(box[1], box[2])
+    if(boxAB < boxBC):
+        boxAB, boxBC = boxBC, boxAB
+    return boxAB
 
-    #Calculate vector end point from 0,0
-    vec[1][0] = vec[1][0] - strPt[0]
-    vec[1][1] = vec[1][1] - strPt[1]
-
-    curMag = dist((0, 0), vec[1])
-
-    vec[1][0] = (vec[1][0]/curMag)*mag + strPt[0]
-    vec[1][1] = (vec[1][1]/curMag)*mag + strPt[1]
-
-    return vec
-
-
-def findline(image):
-
+def findCnts(image):
     # Find contours
     (cnts, _) = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     if len(cnts) > 0:
-        sortedcnts = sorted(cnts, key = cv2.contourArea, reverse = True)
-        #firstcontour = max(cnts, key = cv2.contourArea)
-        #return cv2.minAreaRect(sortedcnts[0])
-        return sortedcnts
+        sortedCnts = sorted(cnts, key = cv2.contourArea, reverse = True)
+        return sortedCnts
 
-i = 0
 while(1):
     print("\n")
     startTime = time.time();
@@ -86,27 +74,27 @@ while(1):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # define range of blue color in HSV
-    #lower_blue = np.array([50,0,235])
-    #upper_blue = np.array([60,10,255])
-    lower_blue = np.array([0, 0, 230])
-    upper_blue = np.array([50, 45, 255])
+    #lowerBlue = np.array([50,0,235])
+    #upperBlue = np.array([60,10,255])
+    lowerBlue = np.array([0, 0, 230])
+    upperBlue = np.array([50, 45, 255])
 
 
     # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask = cv2.inRange(hsv, lowerBlue, upperBlue)
 
-    markers = findline(mask)
+    markers = findCnts(mask)
     if markers is not None:
         boxList = list()
         for marker in markers:
             markerRect = cv2.minAreaRect(marker)
             box = np.int0(cv2.cv.BoxPoints(markerRect))
-            boxab = dist(box[0], box[1])
-            boxbc = dist(box[1], box[2])
-            if(boxab < boxbc):
-                boxab, boxbc = boxbc, boxab
-            boxr = boxab / (boxbc + 1)
-            if(boxr > 2 and boxr < 3.3 and (cv2.contourArea(box) > 80)):
+            boxAB = dist(box[0], box[1])
+            boxBC = dist(box[1], box[2])
+            if(boxAB < boxBC):
+                boxAB, boxBC = boxBC, boxAB
+            boxR = boxAB / (boxBC + 1)
+            if(boxR > 2 and boxR < 3.3 and (cv2.contourArea(box) > 80)):
                 boxList.append(box)
 
         if boxList is not None and len(boxList) > 0:
@@ -116,25 +104,25 @@ while(1):
             for box in boxList:
                 boxAngles.append(boxAng(box))
 
-            xboxlist = sorted(boxList, key = boxcenx)
+            xBoxList = sorted(boxList, key = boxCenX)
 
             dockList = list()
             l = 0
-            for box in xboxlist:
-                if l + 1 < len(xboxlist):
-                    box0ang = boxAng(box)
-                    box1ang = boxAng(xboxlist[l + 1])
-                    print(box0ang, box1ang)
-                    if box0ang > box1ang:
+            for box in xBoxList:
+                if l + 1 < len(xBoxList):
+                    box0Ang = boxAng(box)
+                    box1Ang = boxAng(xBoxList[l + 1])
+                    print(box0Ang, box1Ang)
+                    if box0Ang > box1Ang:
                         dockList.append(l)
                 l += 1
 
-            if dockList is not None:
+            if dockList is not None and len(dockList) > 0:
                 print("dockList length", len(dockList))
 
                 for index in dockList:
-                    origin = average(xboxlist[index])
-                    end = average(xboxlist[index + 1])
+                    origin = average(xBoxList[index])
+                    end = average(xBoxList[index + 1])
                     cv2.line(frame, (origin[0], origin[1]), (end[0], end[1]), (0, 255, 0), 2)
                     center = average((origin, end))
                     lineAngle = math.atan2((origin[0] - end[0]), (origin[1] - end[1]))
@@ -142,23 +130,17 @@ while(1):
                     xComp = int(20 * math.sin(perpAngle)) + center[0]
                     yComp = int(20 * math.cos(perpAngle)) + center[1]
                     cv2.line(frame, (center[0], center[1]), (xComp, yComp), (0, 0, 255), 2)
-                    cv2.drawContours(frame, [xboxlist[index]], -1, (255, 0, 0), 2)
-                    cv2.drawContours(frame, [xboxlist[index + 1]], -1, (255, 0, 0), 2)
+                    cv2.drawContours(frame, [xBoxList[index]], -1, (255, 0, 0), 2)
+                    cv2.drawContours(frame, [xBoxList[index + 1]], -1, (255, 0, 0), 2)
 
-            #cv2.line(frame, (box0cen[0], box0cen[1]), (box1cen[0], box1cen[1]), (0, 255, 0), 2)
-
-            #j = 0
-
-            #for box in boxList:
-            #    if boxAngles[j] <= 90:
-            #        cv2.drawContours(frame, [box], -1, (0, 0, 255), 2)
-            #    else:
-            #        cv2.drawContours(frame, [box], -1, (255, 0, 0), 2)
-            #    j = j + 1
-
-
-    #        sd.putNumber("visionX", x)
-    #        sd.putNumber("visionY", y)
+                focalLength = (64 * 34) / 13
+                leftDistance = (13 * focalLength) / longSide(xBoxList[dockList[0]])
+                rightDistance = (13 * focalLength) / longSide(xBoxList[dockList[0] + 1])
+                dockDistanceCm = (leftDistance + rightDistance) / 2
+                dockXPos = ((boxCenX(xBoxList[dockList[0]]) + boxCenX(xBoxList[dockList[0] + 1])) / 2) - 80
+                dockAngle = dockXPos * 0.3
+                print("Dock angle", dockAngle)
+                print("Dock distance cm", dockDistanceCm)
 
     #        sd.putNumber("Distance", round(cmDistance, 3))
     #        sd.putNumber("visionAngle", angleFromCenter)
